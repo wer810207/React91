99338f45722fc17c914ed30d5d31c881ff3015ac
Yue <yue.lzy@link8tech.tw>
2021-09-01 18:02:34 +0800

    奇怪的專案-9/1

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -21,8 +21,11 @@ function Login() {
         }
     }
     return (
-        <div className="w-full max-w-xs">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div style={{height: '100vh', backgroundColor: '#e1e1e1'}} className="flex items-center justify-center">
+            <form className="bg-white shadow-md rounded px-16 pt-6 pb-8 mb-4">
+                <div className="flex items-center justify-center mb-6">
+                    登入
+                </div>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
                         帳號
@@ -39,13 +42,13 @@ function Login() {
                         className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                         id="password" type="password"/>
                 </div>
-                <div className="flex items-center justify-between">
+                <div className="flex items-center justify-center">
                     <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
                        href="#">
                         註冊
                     </a>
                 </div>
-                <div className="flex items-center justify-between">
+                <div className="flex items-center justify-center mt-6">
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                         type="button">
